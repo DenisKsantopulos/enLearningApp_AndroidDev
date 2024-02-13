2fa829a9cc72971bbb472bb73f9b36914784fe48
DenisKsantopulos <denksant@gmail.com>
2024-02-13 21:27:54 +0300

    secondCommit

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -10,19 +10,6 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val view = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button2)
-
-        button.setOnClickListener{
-            view.text= (view.text.toString().toInt()+1).toString()
-        }
-    }
-
-    fun toastMe(view: View){
-        val myToast = Toast.makeText(this,"Hello Toast!",Toast.LENGTH_SHORT)
-
-        myToast.show()
+        setContentView(R.layout.activity_learn_world)
     }
 }
